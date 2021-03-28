5bda0c4a0c6d9c44c3b1191c6e16d315fa8a12e4
baselkorj <baselanwarkorj@gmail.com>
2021-03-28 22:07:31 +0400

    code overhaul to get live plotting to work

--- main.py
+++ main.py
@@ -1,81 +1,65 @@
 # Live Quadratic Plotter
 # A Tkinter based GUI application for live plotting of quadratic functions using matplotlib.
-
-
-from tkinter import *
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import numpy as np
-
-# Plotting Function
-def plotFunction(a, b, c):
-
-    x = np.linspace(-10, 10, num=1000)  # X limt and values
-    y = [(a * i ** 2 + b * i + c) for i in x]  # Quadratic fucntion
-    figure = plt.figure(
-        figsize=(4, 3), dpi=100
-    )  # plot the figure and define the size and resolution
-    figure.add_subplot(111).plot(x, y)  # plot x and y
-    chart = FigureCanvasTkAgg(figure, master)  # add the figure to the main window
-    chart.get_tk_widget().grid(row=8, column=8)
-    plt.grid()
+import tkinter as tk
 
 
-# GUI Function
-def gui():
-    global s2, s1, s0, master
-    # Basic Window Parameters
-    master = Tk()
-    master.title("LiveQuadraticPloter")
-    master.geometry("800x650")
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        # Creating Variables
+        self.s2 = tk.StringVar()
+        self.s1 = tk.StringVar()
+        self.s0 = tk.StringVar()
 
-    # Create GUI Variables
-    s2 = StringVar()
-    s2.set("")
-    s1 = StringVar()
-    s1.set("")
+        # Trace Function
+        self.s2.trace("w", self.plotFunction)
+        self.s1.trace("w", self.plotFunction)
+        self.s0.trace("w", self.plotFunction)
 
-    s0 = StringVar()
-    s0.set("")
+        # Setting Default Values to 0
+        self.s2.set(0)
+        self.s1.set(0)
+        self.s0.set(0)
 
-    # Labels & Entries
-    s2_label = Label(master, text="S²")
-    s2_label.grid(row=0, column=0)
-    s2_entery = Entry(
-        master, width=15, borderwidth=3, textvariable=s2
-    )  # The 'textvariable' parameter automatically updates variable
-    s2_entery.grid(row=1, column=0)
+        # Labels & Entries
+        self.s2_label = tk.Label(self, text="S²").grid(row=0, column=0, columnspan=1)
+        self.s2_entry = tk.Entry(
+            self, width=15, borderwidth=3, textvariable=self.s2
+        ).grid(row=1, column=0)
 
-    s1_label = Label(master, text="S¹")
-    s1_label.grid(row=2, column=0)
-    s1_entery = Entry(master, width=15, borderwidth=3, textvariable=s1)
-    s1_entery.grid(row=4, column=0)
+        self.s1_label = tk.Label(self, text="S¹").grid(row=2, column=0)
+        self.s1_entry = tk.Entry(
+            self, width=15, borderwidth=3, textvariable=self.s1
+        ).grid(row=3, column=0)
 
-    s0_label = Label(master, text="S⁰")
-    s0_label.grid(row=5, column=0)
-    s0_entery = Entry(master, width=15, borderwidth=3, textvariable=s0)
-    s0_entery.grid(row=6, column=0)
+        self.s0_label = tk.Label(self, text="S⁰").grid(row=4, column=0)
+        self.s0_entry = tk.Entry(
+            self, width=15, borderwidth=3, textvariable=self.s0
+        ).grid(row=5, column=0)
 
-    Btton = Button(
-        master,
-        text="Graph it",
-        command=lambda: plotFunction(
-            int(s2_entery.get()), int(s1_entery.get()), int(s0_entery.get())
-        ),
-    )
+    # Defining Plot Function
+    def plotFunction(self, *args):
+        # Get Variables
+        s2_plot = int(self.s2.get())
+        s1_plot = int(self.s1.get())
+        s0_plot = int(self.s0.get())
 
-    Btton.grid(row=4, column=1)
+        x = np.linspace(-10, 10, num=1000)  # X limit and values
+        y = [
+            (s2_plot * i ** 2 + s1_plot * i + s0_plot) for i in x
+        ]  # Quadratic function
+        figure = plt.figure(
+            figsize=(4, 3), dpi=100
+        )  # plot the figure and define the size and resolution
+        figure.add_subplot(111).plot(x, y)  # plot x and y
+        chart = FigureCanvasTkAgg(figure, self)  # add the figure to the main window
+        chart.get_tk_widget().grid(row=0, column=1, columnspan=2)
+        plt.grid()
 
-    # Trace Functions
-    # Watch the values and execute the plotFunction on change
 
-    # s2.trace("w", plotFunction)
-    # s1.trace("w", plotFunction)
-    # s0.trace("w", plotFunction)
-    master.mainloop()
-
-
-# try to push
-# Start GUI
-gui()
-plotFunction()
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
